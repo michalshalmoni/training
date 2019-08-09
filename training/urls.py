9721d0456cfa5e9ba07cb6c6b29cba062a772a56
michelle shalmoni <michelle@chewse.com>
2019-08-09 16:41:19 -0700

    fix

--- training/urls.py
+++ training/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from views import homePageView
+from training.views import homePageView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
