becb9df380aa8f3083216fe8a1dee72a7fe53923
michelle shalmoni <michelle@chewse.com>
2019-08-12 13:49:06 -0700

    db and template

--- training/views.py
+++ training/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 
 def homePageView(request):
-    return render(request, 'training/templates/counter.html')
+    return render(request, 'training/counter.html')
