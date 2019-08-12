f30337429c322ac9846f18c14237459a09720972
michelle shalmoni <michelle@chewse.com>
2019-08-12 15:14:55 -0700

    db and template

--- training/views.py
+++ training/views.py
@@ -4,9 +4,9 @@ from training.models import Counter
 
 
 def homePageView(request):
-    counter = Counter.objects.get(id=3)
-    counter.value = counter.value+1
-    counter.save()
+    counter = 3#Counter.objects.get(id=3)
+    # counter.value = counter.value+1
+    # counter.save()
 
 
 
