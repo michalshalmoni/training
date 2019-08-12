b64b976d0bc656c4f29cf5c9b8cfe7cbd013c853
michelle shalmoni <michelle@chewse.com>
2019-08-12 15:54:02 -0700

    db and template

--- training/views.py
+++ training/views.py
@@ -5,10 +5,12 @@ from training.models import Counter
 
 def homePageView(request):
     c, created = Counter.objects.get_or_create(value=1, eee=1)
-    c.value = c.value+1
+    if not created:
+        c = Counter.objects.get(id=1)
+    c.value = c.value + 1
     c.save()
 
-    context={
+    context = {
         'Counter': c.value
     }
     return render(request, 'counter.html', context)
