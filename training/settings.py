eb14030b3668db197ef6ffe32bb2e4adc5a4fc2f
michelle shalmoni <michelle@chewse.com>
2019-08-13 09:44:27 -0700

    celery

--- training/settings.py
+++ training/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'training',
-    'django_celery_beat'
+    'training'
 
 ]
 
