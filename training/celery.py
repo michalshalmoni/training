b7436c27072b2942372304c83a72c6b3795883e1
michelle shalmoni <michelle@chewse.com>
2019-08-13 09:43:38 -0700

    celery

--- training/celery.py
+++ training/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 import os
 from celery import Celery
+from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'training.settings')
@@ -13,7 +14,7 @@ app = Celery('training')
 #   should have a `CELERY_` prefix.
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:training.settings')
 
 # this is ncessary for CELERY_ONCE To use REDIS URL
 app.conf.update(
