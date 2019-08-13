e8c04b6e8d6263071c42b3b12490d209b95b99a3
michelle shalmoni <michelle@chewse.com>
2019-08-13 13:17:29 -0700

    scheduled task

--- training/views.py
+++ training/views.py
@@ -18,16 +18,16 @@ def favicon(request):
 
 def homePageView(request):
     from training.tasks import add_to_counter
-    from training.models import Counter,ScheduledCounter
+    from training.models import Counter, ScheduledCounter
+    #calling add to counter async task
     task = add_to_counter.delay()
     result = celery.result.AsyncResult(task.id).state
     while result != 'SUCCESS' and  result != 'FAILURE':
         result = celery.result.AsyncResult(task.id).state
         logger.info('task status' + result)
-
     logger.info('task status' + result)
     c = Counter.objects.get(id=1)
-    s = ScheduledCounter.objects.get(id=1)
+    s = ScheduledCounter.objects.get_or_create(id=1, defaults={'value': 0})
     logger.info('Visitor Number ' + str(c.value) + ' visited the page')
     # raise DatabaseError('fake exception - a very critical issue happened in the db')
 
