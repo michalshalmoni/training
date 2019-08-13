037389b397202d9932b61ccb0d3bf3a8a848abcd
michelle shalmoni <michelle@chewse.com>
2019-08-13 10:20:36 -0700

    celery

--- training/views.py
+++ training/views.py
@@ -18,8 +18,7 @@ def favicon(request):
 
 def homePageView(request):
     from training.tasks import add_to_counter
-    #
-    c = add_to_counter.delay()
+    c = add_to_counter()
     logger.info('Visitor Number ' + str(c.value) + ' visited the page')
     # raise DatabaseError('fake exception - a very critical issue happened in the db')
 
