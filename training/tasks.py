438d9aa3ae013fbb29ed0f591873a7a482f30a6b
michelle shalmoni <michelle@chewse.com>
2019-08-13 09:47:56 -0700

    celery

--- training/tasks.py
+++ training/tasks.py
@@ -1,15 +1,11 @@
 from __future__ import absolute_import
 from celery import shared_task, app
-from celery_once import QueueOnce
-
-import os
 
 import logging
-from celery.decorators import task
 log = logging.getLogger(__name__)
 
 
-@shared_task(base=QueueOnce)
+@shared_task
 def add_to_counter():
     try:
         from training.models import Counter
